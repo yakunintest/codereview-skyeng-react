fc119f92c3c90d8e1fc07af4143f74ccd387535f
Arkadiy <arkadiy.yakunin@skyeng.ru>
2021-12-28 11:51:47 +0300

    Updates

---
+++ precache-manifest.f2dd00c0f8400d4ade56122e0afeafa0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dd3d67d29d998ce5ebe0118c9d92fd8",
+    "url": "/yakunintest/codereview-skyeng-react/index.html"
+  },
+  {
+    "revision": "50ab8e6118b7dae4e2a2",
+    "url": "/yakunintest/codereview-skyeng-react/static/css/main.188f3075.chunk.css"
+  },
+  {
+    "revision": "247e092dd871737f32f6",
+    "url": "/yakunintest/codereview-skyeng-react/static/js/2.47c965e5.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/yakunintest/codereview-skyeng-react/static/js/2.47c965e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50ab8e6118b7dae4e2a2",
+    "url": "/yakunintest/codereview-skyeng-react/static/js/main.9f97d9d5.chunk.js"
+  },
+  {
+    "revision": "8992d44cb12da623dac4",
+    "url": "/yakunintest/codereview-skyeng-react/static/js/runtime-main.94fbffdf.js"
+  }
+]);
