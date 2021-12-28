6b44f7b0538e95137f0f294510a08d5fad04e781
Arkadiy <arkadiy.yakunin@skyeng.ru>
2021-12-28 13:08:18 +0300

    Updates

--- precache-manifest.56e9e5af93dd48f2d9b7a1681dabed03.js
+++ precache-manifest.56e9e5af93dd48f2d9b7a1681dabed03.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2158b11fe1a2a3aa06576c385dac5eb3",
+    "revision": "83dc836464454e1a0e5ea1c77f9dc96a",
     "url": "/codereview-skyeng-react/index.html"
   },
   {
-    "revision": "2b74107461ba7a1034b1",
+    "revision": "d6aa4d79524ef955e3a1",
     "url": "/codereview-skyeng-react/static/css/main.9cea614d.chunk.css"
   },
   {
-    "revision": "247e092dd871737f32f6",
-    "url": "/codereview-skyeng-react/static/js/2.47c965e5.chunk.js"
+    "revision": "9a82dd9a14cf4a5dbfc6",
+    "url": "/codereview-skyeng-react/static/js/2.13b1dd70.chunk.js"
   },
   {
     "revision": "9b318b6fb13190fe82c0677e9264b3c7",
-    "url": "/codereview-skyeng-react/static/js/2.47c965e5.chunk.js.LICENSE.txt"
+    "url": "/codereview-skyeng-react/static/js/2.13b1dd70.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "2b74107461ba7a1034b1",
-    "url": "/codereview-skyeng-react/static/js/main.17fe40d1.chunk.js"
+    "revision": "d6aa4d79524ef955e3a1",
+    "url": "/codereview-skyeng-react/static/js/main.e319aae4.chunk.js"
   },
   {
     "revision": "ee938f409b94dc2b0c22",
